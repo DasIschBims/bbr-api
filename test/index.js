4c796dc1738a6c97d2c506e2336b4c6a85f4c72b
DasIschBims <dasischbims@gmail.com>
2023-07-23 21:54:41 +0200

    update test.js

--- test/index.js
+++ test/index.js
@@ -17,11 +17,13 @@ async function main() {
     const communityServers = serverInfoArr.filter(server => !server.IsOfficial).length;
     const highestTickRate = serverInfoArr.sort((a, b) => b.Hz - a.Hz)[0];
     const serversWithHighestTickRate = serverInfoArr.filter(server => server.Hz === highestTickRate.Hz);
+    const allGamemodes = [...new Set(serverInfoArr.map(server => server.Gamemode))];
 
     console.log(
         `There are \x1b[32m${onlineServers}\x1b[0m online servers with a total of \x1b[32m${totalPlayers}\x1b[0m players.\n\n` +
         `The most popular gamemode is \x1b[32m${mostPopularGamemode}\x1b[0m and the most popular map is \x1b[32m${mostPopularMap}\x1b[0m.\n\n` +
         `There are \x1b[32m${officialServers}\x1b[0m official servers and \x1b[32m${communityServers}\x1b[0m community servers online.\n\n` +
+        `All gamemodes: ${allGamemodes.map(gamemode => '\x1b[32m' + gamemode + '\x1b[0m').join(', ')}\n\n` +
         `The servers with the highest tick rates are:\n${serversWithHighestTickRate.map(server => '\x1b[32m' + server.Hz + 'Hz\x1b[0m - ' + server.Name).join('\n')}`
     );
 }
